3f8866138f46937fb11b5f98c6893b23c1ac084e
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-05-09 16:02:30 +0200

    bottom footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,48 @@
+<footer>
+
+    <div class="container">
+
+        <div class="button">
+
+            <button>SIGN-UP NOW!</button>
+
+        </div>
+
+        <div class="social">
+
+            <div class="follow">
+
+                <span>FOLLOW US</span>
+
+            </div>
+
+            <div class="social-icon">
+
+                <a href="">
+                    <img src="img/footer-facebook.png" alt="">
+                </a>
+
+                <a href="">
+                    <img src="img/footer-twitter.png" alt="">
+                </a>
+
+                <a href="">
+                    <img src="img/footer-youtube.png" alt="">
+                </a>
+
+                <a href="">
+                    <img src="img/footer-pinterest.png" alt="">
+                </a>
+                
+                <a href="">
+                    <img src="img/footer-periscope.png" alt="">
+                </a>
+                
+
+            </div>
+
+        </div>
+
+    </div>
+
+</footer>
